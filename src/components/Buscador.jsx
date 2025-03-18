e483eb33af59bc2388c06cc2039503a88a668db3
Agustin Hahn <agustin.f.hahn@gmail.com>
2025-03-18 11:00:32 -0300

    Update Buscador.jsx

--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -46,7 +46,7 @@ const Buscador = () => {
         placeholder='A QUIEN BUSCAS MASTER? 🧐'
         className='inputBuscador'
         value={filtro}
-        onChange={(e) => setFiltro(e.target.value)}
+        onChange={(e) => setFiltro(e.target.value.replace(/^\s+|\s+$/g, ''))}  // Eliminar solo espacios antes y después
       />
 
       {/* Mostrar resultados */}
